bea83b1dc9fa8d0184cac64f8e50fb8693d4211a
cgil <lao.tseu.is.alive@gmail.com>
2025-04-30 17:58:51 +0200

    preparing test data

--- scripts/extractHashForIdDocs.sh
+++ scripts/extractHashForIdDocs.sh
@@ -28,6 +28,8 @@ done < <(jq -r '.[] | "\(.IDD)|\(.MD5)|\(.SHA256)"' "$JSON_FILE")
 # Output files
 MD5_OUTPUT="md5_output.txt"
 SHA256_OUTPUT="sha256_output.txt"
+ID_OUTPUT="id_output.txt"
+# initialize output files
 echo -n "" > "$MD5_OUTPUT"
 echo -n "" > "$SHA256_OUTPUT"
 echo "# will write md5 in ${MD5_OUTPUT} and sha256 in ${SHA256_OUTPUT}"
@@ -39,5 +41,14 @@ find "$SEARCH_DIR" -type f \( -iname '*.pdf' -o -iname '*.jpg' -o -iname '*.png'
     if [[ -n "${md5_map[$name]}" ]]; then
         echo "${md5_map[$name]}  $filepath" >> "$MD5_OUTPUT"
         echo "${sha256_map[$name]}  $filepath" >> "$SHA256_OUTPUT"
+        echo "$name" >> "$ID_OUTPUT"
     fi
 done
+# next lines are for a special json formated array of records like this :
+# {"IDD":"27201","NVE":"999999","ISP":"1","FIC":"Godoc%2Fmixte%2Fpdf%2F28000%2F27201.pdf","MD5":"4A874CDD01932645DE8CC23A083F8734","SHA256":"953b1b2220e4af679e370eae7f94ffbf02a20419b3387375a66a8c6c22bedafa","TYP":"15"},
+echo "# extracting grep pattern from $ID_OUTPUT"
+sed -E 's/^([0-9]+)$/{"IDD":"&"/' id_output.txt >id_pattern.txt
+echo "# extracting info from ${JSON_FILE} based on existing files in path"
+grep -f id_pattern.txt "$JSON_FILE" |sort -rn >files_info.json
+jq 'map(.id = .IDD| .type = .TYP | if .NVE == "999999" then .version="" else .version=.NVE end |  del(.IDD, .ISP, .NVE, .TYP) )' files_info.json > files_info_formated.json
+
