e6bd9f20085edc826dd5875cbb82d49c3b0f7f44
cgil <lao.tseu.is.alive@gmail.com>
2025-05-12 15:44:08 +0200

    not using matrix

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,6 @@ package version
 var (
 	APP        = "goDirHasher"
 	AppSnake   = "go-dir-hasher"
-	VERSION    = "0.1.3"
+	VERSION    = "0.1.4"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/goDirHasher"
 )
