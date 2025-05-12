7d967544e9056f46cc4bd5f4309b19b984ff9a49
cgil <lao.tseu.is.alive@gmail.com>
2025-05-12 15:24:08 +0200

    added on tag

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,6 @@ package version
 var (
 	APP        = "goDirHasher"
 	AppSnake   = "go-dir-hasher"
-	VERSION    = "0.1.2"
+	VERSION    = "0.1.3"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/goDirHasher"
 )
