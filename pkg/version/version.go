db5c308fd21b772aaf53e4d85613dbe235bcf4de
cgil <lao.tseu.is.alive@gmail.com>
2025-06-18 11:40:40 +0200

    upgraded go version to mitigate  CVE-2025-22874, CVE-2025-0913 and CVE-2025-4673

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,6 +3,8 @@ package version
 var (
 	APP        = "goDirHasher"
 	AppSnake   = "go-dir-hasher"
-	VERSION    = "0.1.5"
+	VERSION    = "0.1.6"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/goDirHasher"
+	REVISION   = "unknown"
+	BuildStamp = "unknown"
 )
