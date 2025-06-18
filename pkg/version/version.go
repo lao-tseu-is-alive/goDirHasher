827d83916325b656767bfae2955cb32a50302a02
cgil <lao.tseu.is.alive@gmail.com>
2025-06-18 12:46:07 +0200

    displaying build stamp

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -3,7 +3,7 @@ package version
 var (
 	APP        = "goDirHasher"
 	AppSnake   = "go-dir-hasher"
-	VERSION    = "0.1.6"
+	VERSION    = "0.1.7"
 	REPOSITORY = "https://github.com/lao-tseu-is-alive/goDirHasher"
 	REVISION   = "unknown"
 	BuildStamp = "unknown"
